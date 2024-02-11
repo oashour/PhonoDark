0a39374df240d98817e9300ff77be9cd37a67aed
Omar Ashour <omar.ashour22@gmail.com>
2024-02-11 14:05:59 -0800

    Matrix implementation of g-functions

--- src/vel_g_function_integrals.py
+++ src/vel_g_function_integrals.py
@@ -20,6 +20,22 @@ def v_minus_func(q_vec, omega, m, vE_vec):
 
     return np.abs(v_star_func(q_vec, omega, m, vE_vec))
 
+
+def matrix_vminus(q, omega, m_chi, v_e):
+    n_modes = omega.shape[1]
+    tiled_q = np.tile(q[None, :, :], (n_modes, 1, 1)).swapaxes(0, 1)
+    q_norm = np.linalg.norm(tiled_q, axis=2)
+    v_star = 1/q_norm*(np.dot(tiled_q, v_e) + q_norm**2/2/m_chi + omega)
+    return np.minimum(np.abs(v_star), const.VESC)
+
+def matrix_g0(q, omega, m_chi, v_e):
+
+    v_minus = matrix_vminus(q, omega, m_chi, v_e)
+    c1 = 2*const.PI**2*const.V0**2/(np.linalg.norm(q, axis=1)*const.N0)
+    c1 = np.tile(c1, (omega.shape[1], 1)).T
+
+    return c1*( np.exp(-v_minus**2/const.V0**2) - np.exp(-const.VESC**2/const.V0**2) )
+
 def g0_func(q_vec, omega, m, vE_vec):
 
     q_mag = np.linalg.norm(q_vec)
